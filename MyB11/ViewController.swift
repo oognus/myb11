bdec208fae8bbf345e6785212627dd61a511bd24
oongus <oognus@gmail.com>
2019-11-15 19:50:13 +0900

    Initial Commit

---
+++ MyB11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyB11
+//
+//  Created by oognus on 2019/11/15.
+//  Copyright © 2019 superjersey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
